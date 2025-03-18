debce36f5ea6ad291fb74f7e65a1236d0ac639ef
yingling21 <eleenchin081@gmail.com>
2025-03-18 11:16:39 +0800

    deleted:    app/favicon.ico 	modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard-map",
+        permanent: true, // true = 308 redirect, false = 307 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
